82cfb326da934dc48a43527cfa5d783f4bcc7401
hijunglit <hijunglit@gmail.com>
2023-10-16 10:44:20 +0900

    component css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Button from './Button';
+import styles from './App.module.css';
+
 function App() {
   return (
     <div>
-      <h1>Hi! good morning again!</h1>
+      <h1 className={styles.title}>Hi! good morning again!</h1>
+      <Button text={"Continue"} /> 
     </div>
   );
 }
