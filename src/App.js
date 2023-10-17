5d0fd21f64587c5838684211b0ab48a399b3e0b6
hijunglit <hijunglit@gmail.com>
2023-10-17 11:52:54 +0900

    movie app - component & routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,6 @@
-import { useEffect, userEffect, useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState([]);
-  const [coins, setConins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
-      .then((response) => response.json())
-      .then((json) => {
-        setConins(json);
-        setLoading(false);
-      });
-  }, []);
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <div>
-          <select>
-            {coins.map((coin) => (
-              <option key={coin.id}>
-                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-              </option>
-            ))}
-          </select>
-          <ul>
-            {coins.map((coin) => (
-              <li key={coin.id}>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD</li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
