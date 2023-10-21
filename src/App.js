fece69d899f55c6309e1b23ecf0795eb5e6ba8b9
hijunglit <hijunglit@gmail.com>
2023-10-21 20:29:40 +0900

    start create-react-app & useState & useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React, { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("Call the API ...");
+  }, []);
   return (
     <div>
-      <h1>Welcome to React</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me!</button>
     </div>
   );
 }
