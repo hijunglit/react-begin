a6f74858001123f85d133af84a68c27876fe7374
hijunglit <hijunglit@gmail.com>
2023-10-21 19:06:25 +0900

    init project for review

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Detail from "./routes/Detail";
-import Home from "./routes/Home";
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/about-us">
-          <h1>Hello</h1>
-        </Route>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <h1>Welcome to React</h1>
+    </div>
   );
 }
 
