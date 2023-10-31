d06accf7cf3d991f96f4e39faf73b65ea5567cb9
hijunglit <hijunglit@gmail.com>
2023-10-31 16:36:41 +0900

    toDo react review

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 function App() {
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
+  const onChange = (event) => setToDo(event.target.value); 
   const onSubmit = (event) => {
     event.preventDefault();
-    if (toDo === "") {
+    if(toDo === "") {
       return;
     }
-    setToDos((currentArray) => [toDo, ... currentArray]);
+    setToDos((currentArray) => [toDo, ...currentArray]);
     setToDo("");
-  };
+  }; 
   console.log(toDos);
   console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
   return (
     <div>
       <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
-        <input
+        <input 
           onChange={onChange}
           value={toDo}
           type='text'
-          placeholder='write your to do ...'
+          placeholder='write here to do'
         />
         <button>Add To Do</button>
       </form>
-      <hr/>
       <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
       </ul>
     </div>
   );
