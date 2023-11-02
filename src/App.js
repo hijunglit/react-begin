473f0bea12ab3fafeaefb50e3cde424673568a53
hijunglit <hijunglit@gmail.com>
2023-11-02 14:46:01 +0900

    movie app review

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Detail from "./routes/Detail";
+import { useEffect, useState } from 'react';
 import Home from './routes/Home';
-
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/about-us">
-          <h1>Hello</h1>
-        </Route>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return null;
 }
 
 export default App;
