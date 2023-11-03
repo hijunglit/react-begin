341610a6d1efe0a8ef3e3d68cf30de33fc2f3f76
hijunglit <hijunglit@gmail.com>
2023-11-04 01:03:13 +0900

    coin tracker

--- src/routes/Crypto.js
+++ src/routes/Crypto.js
@@ -1,18 +1,35 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function Crypto() {
-    const [loading, setLoading] = useState();
-    const getCoins = async() => {
-        const json = (await fetch("https://api.coinpaprika.com/v1/tickers")).json();
-        console.log(json);
-    }
-    useEffect(() => {
-        getCoins();
-    }, []);
-
-    return (
-        <h1>Hi! here is for crypto! We are coming soon!</h1>
-    )
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const getCoins = async () => {
+    const json = await (
+      await fetch("https://api.coinpaprika.com/v1/tickers")
+    ).json();
+    setCoins(json);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getCoins();
+  }, []);
+  console.log(coins);
+  return (
+    <div>
+      <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
 }
 
 export default Crypto;
