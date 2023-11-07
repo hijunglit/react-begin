32ba473b42ca5bcd598e2eced031991017581b5f
hijunglit <hijunglit@gmail.com>
2023-11-07 23:47:13 +0900

    check router for gh-pages

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Crypto from "./routes/Crypto";
 import Detail from "./routes/Detaili";
 import Home from "./routes/Home";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
-      <Router>
-        <Switch>
-          <Route path ="/about-us">
-            <h1>Hello</h1>
-          </Route>
-          <Route path="/coin">
-            <Crypto />
-          </Route>
-          <Route path="/movie/:id">
-            <Detail />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+    <BrowserRouter basename="/react-begin">
+      <Switch>
+        <Route path="/about-us">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/coin">
+          <Crypto />
+        </Route>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
