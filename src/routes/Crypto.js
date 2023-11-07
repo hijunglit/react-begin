57b47b35e47e712e00d120c7739e92ad2282fe2e
hijunglit <hijunglit@gmail.com>
2023-11-07 11:40:15 +0900

    coin tracker complete

--- src/routes/Crypto.js
+++ src/routes/Crypto.js
@@ -1,13 +1,17 @@
+import { number } from 'prop-types';
 import { useState, useEffect } from 'react';
 
 function Crypto() {
     const [loading, setLoading] = useState(true);
     const [coins, setCoins] = useState([]);
-    const [amount, setAmount] =useState(0);
+    const [amount, setAmount] = useState(0);
+    const [selected, setSelected] = useState();
+    const [price, setPrice] = useState();
+    const [state, setState] = useState(false);
+
     async function getCoins() {
         const response = await fetch("https://api.coinpaprika.com/v1/tickers");
         const json = await response.json();
-        console.log(json);
         setLoading(false);
         setCoins(json);
     }
@@ -16,13 +20,19 @@ function Crypto() {
     }, [])
     const onChange = (event) => {
         setAmount(event.target.value);
-
+    }
+    const changeCoin = (event) => {
+        let moneyAmount = event.target.value.split(":");
+        moneyAmount[1] = parseFloat(moneyAmount[1].slice(2, -3));
+        setSelected(moneyAmount[0]);
+        setPrice(moneyAmount[1])
+        setState(true);
     }
     return (
         <div>
             <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
             {loading ? <strong>Loading...</strong> : (
-                <select>
+                <select onChange={changeCoin}>
                     {coins.map((coin) => (
                         <option key={coin.id}>
                             {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
@@ -44,9 +54,7 @@ function Crypto() {
                         </label>
                     </form>
                     <strong>You entered exchange amount of ${amount}USD </strong>
-                    <p>
-                        you can get{coins[0].name} {coins[0].symbol}: {amount / coins[0].quotes.USD.price} bitcoins!
-                    </p>
+                    {state ? <p>you can exchange <strong>"{selected}: ${amount / price} USD"</strong></p>  : ""}
                 </div>
             )}
 
